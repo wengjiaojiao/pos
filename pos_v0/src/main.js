fccc31125bd3c10ab8997283a9224f454d40ae6f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-20 15:26:27 +0800

    keep double decimals,extract subtotal,format code

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,12 +1,16 @@
-//TODO: Please write code in this file.
 function printInventory(inputs) {
-    var info="***<没钱赚商店>购物清单***"+"\n";
-    var sum=0;
-    for(var i=0;i<inputs.length;i++) {
-      info=info+"名称："+inputs[i].name+"，数量：" +inputs[i].count+inputs[i].unit
-      +"，单价："+inputs[i].price+".00(元)，小计："+inputs[i].count*inputs[i].price+".00(元)"+"\n";
-     sum=sum+(inputs[i].price*inputs[i].count);
+    var info = "***<没钱赚商店>购物清单***" + "\n";
+    var sum = 0;
+    for(var i=0; i<inputs.length; i++) {
+        var subtotal = inputs[i].count * inputs[i].price;
+        info +=   "名称：" + inputs[i].name + "，"
+                + "数量：" + inputs[i].count + inputs[i].unit + "，"
+                + "单价：" + inputs[i].price.toFixed(2) + "(元)，"
+                + "小计：" + subtotal.toFixed(2) + "(元)" + "\n";
+        sum += subtotal;
     }
-    info=info+"----------------------"+"\n"+"总计："+sum+".00(元)"+"\n"+"**********************";
+    info += "----------------------" + "\n"
+            + "总计：" + sum.toFixed(2) + "(元)" + "\n"
+            + "**********************";
     console.log(info);
 }
