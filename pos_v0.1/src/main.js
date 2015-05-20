8bd496c0ef4679b6bf55f0d9756a868d14f2a843
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-20 19:40:31 +0800

    format code

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,32 +1,38 @@
 function printInventory(inputs) {
-    var list=[];
-    for (var i=0;i<inputs.length;i++) {
-        var count=1;
-        var temp={};
-        var name=inputs[i].name;
-        var unit=inputs[i].unit;
-        var price=inputs[i].price;
-        var exist=false;
-        for (var k=0;k<list.length;k++) {
-            if(list[k].name==name){
-                list[k].count=list[k].count+count;
-                exist=true;
+    var list = [];
+    for (var i=0; i<inputs.length; i++) {
+        var count = 1;
+        var temp = {};
+        var name = inputs[i].name;
+        var unit = inputs[i].unit;
+        var price = inputs[i].price;
+        var exist = false;
+        for (var k=0; k<list.length; k++) {
+            if(list[k].name === name) {
+                list[k].count = list[k].count + count;
+                exist = true;
             }
         }
         if(!exist) {
-            temp.name=name;
-            temp.count=count;
-            temp.unit=unit;
-            temp.price=price;
-            list.push(temp);  
+            temp.name = name;
+            temp.count = count;
+            temp.unit = unit;
+            temp.price = price;
+            list.push(temp);
         }
     }
-    var info="***<没钱赚商店>购物清单***"+"\n";
-    var sum=0;
-    for(var i=0;i<list.length;i++) {
-     info=info+"名称："+list[i].name+"，数量：" +list[i].count+list[i].unit+"，单价："+list[i].price+".00(元)，小计："+list[i].count*list[i].price+".00(元)"+"\n";
-     sum=sum+(list[i].price*list[i].count);
+    var info = "***<没钱赚商店>购物清单***" + "\n";
+    var sum = 0;
+    for(var i=0; i<list.length; i++) {
+        var subtotal = list[i].count * list[i].price;
+        info += "名称：" + list[i].name + "，"+
+                "数量：" + list[i].count + list[i].unit + "，"+
+                "单价：" + list[i].price.toFixed(2) + "(元)，"+
+                "小计：" + subtotal.toFixed(2) + "(元)" + "\n";
+        sum += subtotal;
     }
-    info=info+"----------------------"+"\n"+"总计："+sum+".00(元)"+"\n"+"**********************";
+    info += "----------------------" + "\n" +
+            "总计：" + sum.toFixed(2) + "(元)" + "\n" +
+            "**********************";
     console.log(info);
 }
