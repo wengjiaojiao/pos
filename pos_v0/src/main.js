25bd931d33a2b6be479e485c8cf850b82139b27d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-21 15:10:01 +0800

    formate code again

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -3,10 +3,10 @@ function printInventory(inputs) {
     var sum = 0;
     for(var i=0; i<inputs.length; i++) {
         var subtotal = inputs[i].count * inputs[i].price;
-        info +=   "名称：" + inputs[i].name + "，"
-                + "数量：" + inputs[i].count + inputs[i].unit + "，"
-                + "单价：" + inputs[i].price.toFixed(2) + "(元)，"
-                + "小计：" + subtotal.toFixed(2) + "(元)" + "\n";
+        info += "名称：" + inputs[i].name + "，"
+              + "数量：" + inputs[i].count + inputs[i].unit + "，"
+              + "单价：" + inputs[i].price.toFixed(2) + "(元)，"
+              + "小计：" + subtotal.toFixed(2) + "(元)" + "\n";
         sum += subtotal;
     }
     info += "----------------------" + "\n"
