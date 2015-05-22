328893e6df19485490a60fc4bfa98851ec8335a9
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-22 11:13:37 +0800

    console currentdate

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,1 +1,16 @@
-//TODO: Please write code in this file.
+function printInventory(inputs){
+    var dateDigitToString;
+
+    dateDigitToString = function (num) {
+        return num < 10 ? '0' + num : num;
+    };
+    var currentDate = new Date(),
+        year = dateDigitToString(currentDate.getFullYear()),
+        month = dateDigitToString(currentDate.getMonth() + 1),
+        date = dateDigitToString(currentDate.getDate()),
+        hour = dateDigitToString(currentDate.getHours()),
+        minute = dateDigitToString(currentDate.getMinutes()),
+        second = dateDigitToString(currentDate.getSeconds()),
+        formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+        console.log(formattedDateString);
+}
