1575185110494788c4fa0f65a6b9b1fbce042695
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-20 16:45:54 +0800

    create cartItem function

--- pos_v2/src/model/cartItem.js
+++ pos_v2/src/model/cartItem.js
@@ -3,9 +3,11 @@ function CartItem(item, count) {
     this.item = item;
     this.count = count;
 }
+
 CartItem.prototype.getItem = function() {
     return this.item;
 }
+
 CartItem.prototype.getCount = function() {
     return this.count;
 }
