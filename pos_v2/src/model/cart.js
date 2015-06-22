257c71bc4d26f490a36ce139c538bc43537c5c1a
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-22 11:40:07 +0800

    realize calculate subtotal without promotion

--- pos_v2/src/model/cart.js
+++ pos_v2/src/model/cart.js
@@ -1,7 +1,24 @@
+
 function Cart() {
 
 }
-Cart.prototype.getSum = function() {
-    
+Cart.prototype.getSubtotal = function(cartitem) {
+    var subtotal = [];
+
+    each(cartitem, function(value, key) {
+        subtotal.push(cartitem[key].item.price * cartitem[key].count);
+    })
+    return subtotal;
+}
+function each(collection,fun) {
+    if (Array.isArray(collection)) {
+        for (var i = 0; i < collection.length; i++) {
+            fun(collection[i], i);
+        }
+    }else {
+        for (var key in collection) {
+            fun(collection[key], key);
+        }
+    }
 }
 module.exports = Cart;
