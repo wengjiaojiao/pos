ec7869dce41a0d0f124dc0504bc1adbf8c1f14b6
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-22 20:15:38 +0800

    add gettotal function to get and output the lasttotal

--- pos_v2/src/model/pos.js
+++ pos_v2/src/model/pos.js
@@ -1,22 +1,46 @@
-var cartitem = require('./scanner');
 var Dateformatter = require('../tools/dateformatter');
-
 
 function Pos() {
 
 }
-Pos.prototype.getInfo = function (cartitem) {
+Pos.prototype.getInfo = function (cartitems, subtotal, promotionitems,promotioncount) {
     var getdate = new Dateformatter().getDate();
     var result;
     result = '***<没钱赚商店>购物清单***\n' +
                  '打印时间：' + getdate + '\n----------------------\n';
-    each(cartitem, function(value, key) {
-        result += '名称：' + cartitem[key].item.name +
-                '，数量：' + cartitem[key].count + cartitem[key].item.unit +
-                '，单价：' + cartitem[key].item.price.toFixed(2) + '(元)' +
-                '，小计：' + '(元)\n';
-    })
+    each(cartitems, function(cartitem, key) {
+        result += '名称：' + cartitem.item.name +
+                '，数量：' + cartitem.count + cartitem.item.unit +
+                '，单价：' + cartitem.item.price.toFixed(2) + '(元)' +
+                '，小计：' + subtotal[key].toFixed(2) + '(元)\n';
+    });
+    result += '----------------------\n' + '挥泪赠送商品：\n';
+    each(promotionitems,function(promotionitem, key) {
+        result += '名称：' + promotionitem.item.name +
+                '，数量：' + promotioncount[key]
+                           + promotionitem.item.unit + '\n';
+    });
+    result +=  '----------------------\n' +
+                this.getTotal(cartitems, subtotal) +
+                '**********************';
+
     return result;
+}
+
+Pos.prototype.getTotal = function (cartitems, subtotals) {
+    var total = 0;
+    var lastTotal = 0;
+    var save = 0;
+    each(cartitems, function(cartitem) {
+        total += cartitem.item.price * cartitem.count;
+    });
+
+    each(subtotals, function(subtotal) {
+        lastTotal += subtotal;
+    });
+
+    return '总计：' + (lastTotal).toFixed(2) + '(元)\n' +
+            '节省：' + (total - lastTotal).toFixed(2) + '(元)\n'
 }
 
 function each(collection,fun) {
