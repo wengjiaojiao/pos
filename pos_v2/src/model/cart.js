ddcf3666664e34be0eca07dbba2a5441497d82f8
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-22 20:13:51 +0800

    add getcartitemprice function to calculate the promotionprice

--- pos_v2/src/model/cart.js
+++ pos_v2/src/model/cart.js
@@ -1,15 +1,33 @@
+var fixtures = require('../../spec/fixtures');
 
 function Cart() {
 
 }
-Cart.prototype.getSubtotal = function(cartitem) {
+Cart.prototype.getSubtotal = function(cartitems) {
     var subtotal = [];
+    var promotions = fixtures.loadPromotions();
+    var that = this;
 
-    each(cartitem, function(value, key) {
-        subtotal.push(cartitem[key].item.price * cartitem[key].count);
-    })
+    each(cartitems, function(cartitem, key) {
+        subtotal.push((cartitem.item.price * cartitem.count) - that.getCartItemPrice(cartitem));
+    });
+
     return subtotal;
 }
+
+Cart.prototype.getCartItemPrice = function(cartitem) {
+    var promotionprice = 0;
+    var promotions = fixtures.loadPromotions();
+
+    each(promotions[0].barcodes, function(promotion){
+        if(cartitem.item.barcode === promotion) {
+            promotionprice = parseInt(cartitem.count/3) * cartitem.item.price;
+        }
+    });
+
+    return promotionprice;
+}
+
 function each(collection,fun) {
     if (Array.isArray(collection)) {
         for (var i = 0; i < collection.length; i++) {
