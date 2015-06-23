67717f41150d4f472e859966bfdeea8bb31e66df
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-23 19:27:40 +0800

    remove my lodash each

--- pos_v2/src/model/pos.js
+++ pos_v2/src/model/pos.js
@@ -8,14 +8,14 @@ Pos.prototype.getInfo = function(cartitems, subtotal, promotionitems,promotionco
     var result;
     result = '***<没钱赚商店>购物清单***\n' +
                  '打印时间：' + getdate + '\n----------------------\n';
-    each(cartitems, function(cartitem, key) {
+    cartitems.forEach(function(cartitem, key) {
         result += '名称：' + cartitem.item.name +
                 '，数量：' + cartitem.count + cartitem.item.unit +
                 '，单价：' + cartitem.item.price.toFixed(2) + '(元)' +
                 '，小计：' + subtotal[key].toFixed(2) + '(元)\n';
     });
     result += '----------------------\n' + '挥泪赠送商品：\n';
-    each(promotionitems,function(promotionitem, key) {
+    promotionitems.forEach(function(promotionitem, key) {
         result += '名称：' + promotionitem.item.name +
                 '，数量：' + promotioncount[key]
                            + promotionitem.item.unit + '\n';
@@ -31,27 +31,16 @@ Pos.prototype.getTotal = function(cartitems, subtotals) {
     var total = 0;
     var lastTotal = 0;
     var save = 0;
-    each(cartitems, function(cartitem) {
+    cartitems.forEach(function(cartitem) {
         total += cartitem.item.price * cartitem.count;
     });
 
-    each(subtotals, function(subtotal) {
+    subtotals.forEach(function(subtotal) {
         lastTotal += subtotal;
     });
 
     return '总计：' + (lastTotal).toFixed(2) + '(元)\n' +
-            '节省：' + (total - lastTotal).toFixed(2) + '(元)\n'
+           '节省：' + (total - lastTotal).toFixed(2) + '(元)\n';
 }
 
-function each(collection,fun) {
-    if (Array.isArray(collection)) {
-        for (var i = 0; i < collection.length; i++) {
-            fun(collection[i], i);
-        }
-    }else {
-        for (var key in collection) {
-            fun(collection[key], key);
-        }
-    }
-}
 module.exports = Pos;
