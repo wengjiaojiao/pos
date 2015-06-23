077f6281c65fff7b2cce791bc25ca46778a75e4f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-23 17:41:36 +0800

    remove the getCount and getItem

--- pos_v2/src/model/cartItem.js
+++ pos_v2/src/model/cartItem.js
@@ -1,14 +1,44 @@
+var fixtures = require('../../spec/fixtures');
 
 function CartItem(item, count) {
     this.item = item;
     this.count = count;
 }
-CartItem.prototype.getItem = function() {
-    return this.item;
+
+CartItem.prototype.getSubtotal = function(cartitems) {
+    var subtotal = [];
+    var promotions = fixtures.loadPromotions();
+    var that = this;
+
+    each(cartitems, function(cartitem, key) {
+        subtotal.push((cartitem.item.price * cartitem.count) - that.getCartItemPrice(cartitem));
+    });
+
+    return subtotal;
 }
 
-CartItem.prototype.getCount = function() {
-    return this.count;
+CartItem.prototype.getCartItemPrice = function(cartitem) {
+    var promotionprice = 0;
+    var promotions = fixtures.loadPromotions();
+
+    each(promotions[0].barcodes, function(promotion){
+        if(cartitem.item.barcode === promotion) {
+            promotionprice = parseInt(cartitem.count/3) * cartitem.item.price;
+        }
+    });
+
+    return promotionprice;
 }
 
+function each(collection,fun) {
+    if (Array.isArray(collection)) {
+        for (var i = 0; i < collection.length; i++) {
+            fun(collection[i], i);
+        }
+    }else {
+        for (var key in collection) {
+            fun(collection[key], key);
+        }
+    }
+}
 module.exports = CartItem;
